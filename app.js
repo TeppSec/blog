e323ffe3678f738c4fc4ffd2bebaa917625d01ae
Oliver <oltepper@gmail.com>
2022-08-15 13:21:16 +0200

    make it better

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
+const _ = require('lodash');
+
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 const aboutContent =
@@ -47,6 +49,20 @@ app.post('/compose', (req, res) => {
   };
   posts.push(post);
   res.redirect('/');
+});
+
+app.get('/posts/:postName', (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+    if (requestedTitle === storedTitle) {
+      res.render('post', {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
